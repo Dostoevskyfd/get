007e7f332c6b055358b80ef69d8e93298824873f
Dostoevskyfd <zhogli.morax.qiqi@gmial.com>
2024-09-19 13:02:48 +0500

    cre

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <h1> Главная старница</h1>
+    <a href="{{router (frist)}}"></a>
+    <p>1.	Открыть файл httpd.conf (D:\xampp\apache\conf\httpd.conf) проверить что строка раскомментирована</p>
+</body>
+</html>
